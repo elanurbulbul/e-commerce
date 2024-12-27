de6f971ba4d08fb9cfacbae292912cdcde9163dd
elanurbulbul <elanrbulbul@gmail.com>
2024-12-27 20:25:49 +0300

    CreatedFolders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./style.css";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div class="text-3xl font-bold underline">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

6918780574a25208ea87417cefdba13db21528df
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-12-28 23:18:00 +0300

    CreatedReduxTK

---
+++ src/redux/categorySlice.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+//baslangıc durumu
+const initialState = {
+    categories: [],
+}
+export const getCategories = createAsyncThunk('category', async () => {
+    const response = await fetch("https://fakestoreapi.com/products/categories")
+    const data = response.json()
+    return data
+})
+
+const categorySlice = createSlice({
+    name: "categories",
+    initialState,
+    //api cektiğim için burda actionlara gerek yok
+    reducers: {},
+    extraReducers: (builder) => { 
+        builder
+        .addCase(getCategories.fulfilled, (state, action) => {
+            state.categories = action.payload
+            //state altındakı categories arrayine action.payloadı atıyoruz
+        })
+    }
+})
+export default categorySlice.reducer

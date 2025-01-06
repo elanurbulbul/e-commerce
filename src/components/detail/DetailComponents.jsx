3b44b116ed923a7da123fdf43de1412436535198
elanurbulbul <elanrbulbul@gmail.com>
2025-01-07 01:19:13 +0300

    CreatedCartSlice

--- src/components/detail/DetailComponents.jsx
+++ src/components/detail/DetailComponents.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../../redux/cartSlice';
 
 const DetailComponents = ({productDetail}) => {
+  const dispatch = useDispatch();
+  const [quantity, setQuantity] = React.useState(0)
+
+  const decrement = () => {
+    if(quantity > 0){
+        setQuantity(quantity - 1)
+    }
+  }
+  const increment = () => {
+    if(quantity < productDetail.rating.count){
+        setQuantity(quantity + 1)
+    }
+  }
+  const addBasket = () => {
+    dispatch(addToCart({id: productDetail?.id, title: productDetail?.title, image: productDetail?.image, price: productDetail?.price, quantity:quantity}))
+  }
+
   return (
     <div className='flex gap-10 my-10'>
-        <img className='w-[400px] f-[400px] ' src={productDetail?.image} alt={productDetail.title} />
-        <div className='flex flex-col gap-2'>
+        <img className='w-[700px] h-[500px]  ' src={productDetail?.image} alt={productDetail.title} />
+        <div className=''>
         <div className='text-4xl font-bold'>{productDetail?.title}</div>
-        <div className='my-2'>{productDetail?.description}</div>
-        <div className=''>Rating: {productDetail?.rating?.rate}</div>
-        <div className=''>Count: {productDetail?.rating?.count}</div>
-        <div className='text-3xl'>Price: {productDetail?.price}
-            <span className='text-sm'> TL</span>
+        <div className='my-2 text-2xl'>{productDetail?.description}</div>
+        <div className='my-2 text-xl text-red-950'>Rating: {productDetail?.rating?.rate}</div>
+        <div className='my-2 text-xl text-red-950'>Count: {productDetail?.rating?.count}</div>
+        <div className='text-5xl font-bold'>Price: {productDetail?.price} <span className='text-sm'> TL</span> </div>
+        <div className='flex items-center gap-5 my-4'>
+            <div onClick={decrement} className='bg-red-950 text-white p-2 text-5xl'>-</div>
+            <input type='text' value={quantity} className='w-12 text-center font-bold text-4xl' />
+            <div onClick={increment} className='bg-red-950 text-white p-2 text-5xl'>+</div>
         </div>
-
+        <div onClick={addBasket} className='my-4 w-[200px] border cursor-pointer h-16 flex items-center bg-gray-200  justify-center '>
+            Add Basket
+        </div>
         </div>
    
     </div>
---
+++ src/redux/cartSlice.js
@@ -0,0 +1,84 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+//verileri çağırdığımızda local storage'dan verileri almak için bir fonksiyon oluşturuyoruz
+const fetchFromLocalStorage = () => {
+    let cart = localStorage.getItem('cart')//get the cart from local storage
+    if (cart) {
+        return JSON.parse(localStorage.getItem('cart'))
+    }
+    else {
+        return [] //return empty array if nos cart is found in local storage
+    }
+}
+//verileri local storage'a kaydetmek için bir fonksiyon oluşturuyoruz
+const storeInLocalStorage = (data) => {
+    localStorage.setItem('cart', JSON.stringify(data))
+}
+
+
+const initialState = {
+    carts:fetchFromLocalStorage() ,
+    itemCount: 0,
+    totalAmount: 0,
+}
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    //normalde extrareducer kullanırdık ama burada api üzerinden veri çekmediğimiz için direkt reducers kullanıyoruz
+    //local storage'a veri eklemek veya stateleri güncellemek için addToCart fonksiyonunu oluşturuyoruz
+    reducers:{
+        addToCart: (state, action) => {
+            //bir ürün eklediğimizde state'i güncelliyoruz
+            //bir ürün karta eklenmişse tekrar eklenmez state'i güncellemek yerine sadece miktarı arttırıyoruz 
+            //verileri alıp map leriz
+            const isItemCart = state.carts.find (item => item.id === action.payload.id)
+            // herbir elemanı tek tek kontrol eder ve eğer aynı id varsa miktarı arttırır, hr gezdiğin elemanın id değeri action.payload.id'ye eşitse yani dısarıdan gelen id değerine
+            if(isItemCart){
+                const tempCart = state.carts.map(item => {
+                //state cartları tek tek gez ve maple    
+                    if(item.id === action.payload.id){
+                    //bu iki değer eşitse miktarı arttır
+                        let temQty = item.quantity + action.payload.quantity
+                        //her urunun guantitiy'si olacak onları artırır
+                        let tempTotalPrice = temQty + item.price
+                        //her ürünün fiyatını artırır
+                        return {...item, quantity: temQty, totalPrice: tempTotalPrice} 
+                    }
+                    else{
+                        return item
+                        //eğer aynı id yoksa aynı ürünü geri döndür
+                    }
+                })
+                state.carts = tempCart //state'i güncelle  
+                storeInLocalStorage(state.carts) //local storage'a güncel verileri kaydet
+            }  
+            else{
+                state.carts.push(action.payload) //eğer aynı id yoksa yeni ürünü ekler
+                storeInLocalStorage(state.carts) //local storage'a güncel verileri kaydet
+            }  
+        },
+        removeFromCart: (state, action) => {
+            const tempCart = state.carts.filter(item => item.id !== action.payload.id)
+            //itemların id'si action.payload.id'ye eşit değilse filtrele
+            state.carts = tempCart
+            storeInLocalStorage(state.carts)
+            //ürünü karta eklediğimizde state'i güncelliyoruz
+        },
+        //silme işlemi yaparken dısarıdan bi id göndericz onun üzerinden filtreleme yapıcak, farklı olanları filtrele aynı olanları sil
+        
+        clearCart: (state) => {
+            state.carts = []
+            storeInLocalStorage(state.carts)
+        },
+        getCartTotal: (state) => {
+            state.totalAmount = state.carts.reduce((cartTotal, cartItem) =>
+            {
+                return cartTotal += cartItem.totalPrice
+            },0)
+            state.itemCount = state.carts.length
+        }
+    }
+})
+
+export const {addToCart, removeFromCart, clearCart, getCartTotal} = cartSlice.actions
+export default cartSlice.reducer
